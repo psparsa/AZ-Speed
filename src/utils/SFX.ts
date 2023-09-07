617733bb3c5f7bcdd24f3afc0a4ba79432e41fc7
PS-PARSA <57572461+psparsa@users.noreply.github.com>
2023-09-07 11:42:39 +0330

    style(SFX.ts): code format

--- src/utils/SFX.ts
+++ src/utils/SFX.ts
@@ -3,6 +3,12 @@ import { Howl } from 'howler';
 export const SFX = {
   click: new Howl({ src: ['/sfx/click.wav'], html5: true }),
   wrong: new Howl({ src: ['/sfx/wrong.wav'], html5: true }),
-  backspace: new Howl({ src: ['/sfx/backspace.wav'], html5: true }),
-  restart: new Howl({ src: ['/sfx/restart.wav'], html5: true }),
+  backspace: new Howl({
+    src: ['/sfx/backspace.wav'],
+    html5: true,
+  }),
+  restart: new Howl({
+    src: ['/sfx/restart.wav'],
+    html5: true,
+  }),
 };
